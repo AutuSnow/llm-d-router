0d7be92503d2609c20051bfcbede82658d3a3010
Maroon Ayoub <maroon.ayoub@ibm.com>
2025-04-13 20:30:50 +0300

    - reset makefile - reset linter config - reset hello files (andy)
    Signed-off-by: Maroon Ayoub <maroonay@gmail.com>

--- pkg/client/kvblock-scorer_test.go
+++ pkg/client/kvblock-scorer_test.go
@@ -1,14 +1,16 @@
-package client
+package client_test
 
 import (
 	"testing"
+
+	"github.com/neuralmagic/distributed-kv-cache/pkg/client"
 
 	"github.com/stretchr/testify/assert"
 )
 
 // TestLongestPrefixScorer verifies scoring based on consecutive block hits from the start.
 func TestLongestPrefixScorer(t *testing.T) {
-	scorer := &LongestPrefixScorer{}
+	scorer := &client.LongestPrefixScorer{}
 	blockKeys := []string{"b1", "b2", "b3", "b4", "b5", "b6"}
 
 	hitmap := map[string]string{
@@ -34,7 +36,7 @@ func TestLongestPrefixScorer(t *testing.T) {
 
 // TestHighestBlockHitScorer verifies scoring based on the highest index where a pod has a block.
 func TestHighestBlockHitScorer(t *testing.T) {
-	scorer := &HighestBlockHitScorer{}
+	scorer := &client.HighestBlockHitScorer{}
 	blockKeys := []string{"b1", "b2", "b3", "b4", "b5"}
 
 	hitmap := map[string]string{
@@ -58,7 +60,7 @@ func TestHighestBlockHitScorer(t *testing.T) {
 
 // TestCoverageBasedScorer verifies scoring based on total number of non-consecutive block hits.
 func TestCoverageBasedScorer(t *testing.T) {
-	scorer := &CoverageBasedScorer{}
+	scorer := &client.CoverageBasedScorer{}
 	blockKeys := []string{"b1", "b2", "b3", "b4", "b5"}
 
 	hitmap := map[string]string{
